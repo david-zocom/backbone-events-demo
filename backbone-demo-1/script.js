b14ab0b8f025fb6ac9ae66bc700de8d4ffb42b37
David Andersson <David Andersson>
2019-02-20 10:45:58 +0100

    Goat shop with basic model

--- backbone-demo-1/script.js
+++ backbone-demo-1/script.js
@@ -1,1 +1,60 @@
-const data = null;
+const data = {
+	cart: {
+		numberOfItems: 0
+	}
+};
+
+const CartModel = Backbone.Model.extend({
+	defaults: {
+		numberOfItems: 0
+	}
+});
+
+let cartModelInstance = new CartModel({});
+console.log(cartModelInstance.get('numberOfItems'));
+
+const CartView = Backbone.View.extend({
+	render: function() {
+		let numberOfItems = this.model.get('numberOfItems');
+		if( numberOfItems === 0 ) {
+			this.$el.html(`Kundvagnen är tom :(`);
+		} else if( numberOfItems === 1 ) {
+			this.$el.html(`Kundvagnen innehåller ${numberOfItems} get!`);
+		} else {
+			this.$el.html(`Kundvagnen innehåller ${numberOfItems} getter!`);
+		}
+	}
+});
+
+
+
+$(document).ready(function() {
+	let cartViewInstance = new CartView({
+		el: '.cartContainer',
+		model: cartModelInstance
+	});
+
+	// cartViewInstance.$el = ...
+	cartViewInstance.render();
+
+	$('#addGoatToCart').on('click', function(event) {
+		let count = cartModelInstance.get('numberOfItems');
+		cartModelInstance.set({ numberOfItems: count + 1 });
+		cartViewInstance.render();
+		if( count === 0 ) {
+			$('#removeGoatFromCart').prop( "disabled", false );
+		}
+	});
+	$('#removeGoatFromCart').on('click', function(event) {
+		let count = cartModelInstance.get('numberOfItems');
+		cartModelInstance.set({ numberOfItems: count - 1 });
+		cartViewInstance.render();
+		if( count <= 1 ) {
+			$('#removeGoatFromCart').prop( "disabled", true );
+		}
+	});
+});
+
+
+
+//
